8506f2509c30eb7dc2d3f1479f7fd168d865a7ea
Dat <ltdat01042002@gmail.com>
2023-06-21 11:03:21 +0700

    EX16

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddUser from './components/AddUser';
+import User from './components/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row mt-5'>
+        <div className='col-12 d-flex'>
+          <div className='col-md-5'>
+          <AddUser></AddUser>
+          </div>
+          <div className='col-md-7'>
+          <User></User>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
